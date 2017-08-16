6f83c440798ec457fab3631ee2e8f2e628b5f8e9
Patrick McGuire <insidenothing@gmail.com>
2017-08-16 13:53:51 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
-date_default_timezone_set("Asia/America/New_York");
+date_default_timezone_set("America/America/New_York");
 // Shortname
 $shortname = "mdpp";
 //The server host is the IP or DNS of the IRC server. 
