6905c01ce85c23d3d7852d6b1033fda6dd4f532b
Patrick McGuire <insidenothing@gmail.com>
2017-08-16 15:14:33 -0400

    Update bot.php

--- bot.php
+++ bot.php
@@ -64,31 +64,19 @@ if($server['SOCKET']){
     //
     $pos = strpos($server['READ_BUFFER'], 'start meeting');
     if ($pos !== false){  
-      SendCommand("PRIVMSG #mdpp :Meeting log has started");  
-      //SendCommand("NAMES #mdpp");
+      //SendCommand("PRIVMSG #mdpp :Meeting log has started");  
+      SendCommand("NAMES #mdpp");
       touch($lockfile);
       error_log("<div style='border:1px solid black; padding:5px; background-color:lightgreen;'>".date('r')." Meeting Has Started</div>", 3, $meeting_log);
       $refresh = '<meta http-equiv="refresh" content="5">';
       error_log($refresh, 3, $meeting_log);
     }
     //
-    //// names command
-    //
-    $pos = strpos($server['READ_BUFFER'], 'roll call');
-    if ($pos !== false){  
-      //SendCommand("PRIVMSG #mdpp :Meeting log has started");  
-      SendCommand("NAMES #mdpp");
-      //touch($lockfile);
-      //error_log("<div style='border:1px solid black; padding:5px; background-color:lightgreen;'>".date('r')." Meeting Has Started</div>", 3, $meeting_log);
-      //$refresh = '<meta http-equiv="refresh" content="5">';
-      //error_log($refresh, 3, $meeting_log);
-    }
-    //
     //// end command
     //
     $pos = strpos($server['READ_BUFFER'], 'end meeting');
     if ($pos !== false){ 
-      SendCommand("PRIVMSG #mdpp :Meeting log has ended");  
+      //SendCommand("PRIVMSG #mdpp :Meeting log has ended");  
       unlink($lockfile);
       error_log("<div style='border:1px solid black; padding:5px; background-color:lightblue;'>".date('r')." Meeting Complete </div>", 3, $meeting_log);
     }
