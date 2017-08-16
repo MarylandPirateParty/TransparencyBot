997603503e90944d1ecc10b11f9c89cd2f8cdc80
Patrick McGuire <insidenothing@gmail.com>
2017-08-16 13:14:27 -0400

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,13 +26,15 @@ if($server['SOCKET']){
   //while we are connected to the server 
   while(!feof($server['SOCKET'])){ 
     $server['READ_BUFFER'] = fgets($server['SOCKET'], 1024); //get a line of data from the server 
-    echo "[RECIVE] ".$server['READ_BUFFER']."<br>\n\r"; //display the recived data from the server 
+    //echo "[RECIVE] ".$server['READ_BUFFER']."<br>\n\r"; //display the recived data from the server 
     if($logging == 'on'){
       $parts = explode("!",$server['READ_BUFFER']);
       $name = $parts[0];
       $parts = explode(":",$server['READ_BUFFER']); 
       $message = $parts[2];
       error_log(date('r')." $name: $message \n", 3, $meeting_log); 
+    }else{
+      error_log(date('r')." [receive] [".$server['READ_BUFFER']."] \n", 3, $debug_log);
     }
     /* 
     IRC Sends a "PING" command to the client which must be anwsered with a "PONG" 
@@ -42,25 +44,30 @@ if($server['SOCKET']){
     $i++;
     // line 90 is about mid MOTD - we have found it is good to send the commands at this point
     if($i == 90){ 
-      //If we have joined the server 
       SendCommand("JOIN #mdpp\n\r"); //Join the chanel 
     }
+    //
+    //// first command
+    //
     $pos = strpos($server['READ_BUFFER'], 'start meeting');
     if ($pos !== false){ 
       SendCommand("NAMES #mdpp"); 
       SendCommand("PRIVMSG #mdpp :Meeting log has started"); 
       $logging='on';  
-      error_log("\n\n".date('r')." Meeting Log Active\n", 3, "mdpp.log");
+      error_log("\n\n".date('r')." Meeting Has Started\n", 3, $meeting_log);
     }
+    //
+    //// second command
+    //
     $pos = strpos($server['READ_BUFFER'], 'end meeting');
     if ($pos !== false){ 
       SendCommand("PRIVMSG #mdpp :Meeting log has ended");  
-      $logging='off';  
+      $logging='off';
+      error_log("\n\n".date('r')." Meeting Complete\n", 3, $meeting_log);
     }
     //If the server has sent the ping command 
     if(substr($server['READ_BUFFER'], 0, 6) == "PING :"){ 
-      SendCommand("PONG :".substr($server['READ_BUFFER'], 6)."\n\r"); //Reply with pong 
-      //SendCommand("PRIVMSG #mdpp :Meeting log function available");                                                                                                                                                                                                                                                                                                                     //As you can see i dont have it reply with just "PONG" 
+      SendCommand("PONG :".substr($server['READ_BUFFER'], 6)."\n\r"); //Reply with pong                                                                                                                                                                                                                                                                                                                      //As you can see i dont have it reply with just "PONG" 
       //It sends PONG and the data recived after the "PING" text on that recived line 
       //Reason being is some irc servers have a "No Spoof" feature that sends a key after the PING 
       //Command that must be replied with PONG and the same key sent. 
@@ -70,9 +77,11 @@ if($server['SOCKET']){
 } // end if socket exists
 
 function SendCommand ($cmd){ 
+  global $debug_log;
   global $server; //Extends our $server array to this function 
   @fwrite($server['SOCKET'], $cmd, strlen($cmd)); //sends the command to the server 
-  echo "[SEND] $cmd <br>"; // displays it to the screen 
+  //echo "[SEND] $cmd <br>"; // displays it to the screen 
+  error_log(date('r')." [send] [$cmd] \n", 3 , $debug_log);
 } 
 
 ?>
