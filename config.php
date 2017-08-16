b975d0f55e08e5c09c4a44ccc9be8350e42b3fa2
Patrick McGuire <insidenothing@gmail.com>
2017-08-16 14:44:43 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,12 +2,13 @@
 date_default_timezone_set("America/New_York");
 // Shortname
 $shortname = "mdpp";
+$fullname = "Maryland Pirate Party";
 //The server host is the IP or DNS of the IRC server. 
 $server_host = "irc.pirateirc.net"; 
 //Server Port, this is the port that the irc server is running on. Deafult: 6667 
 $server_port = 6667; 
 //Server Chanel, After connecting to the IRC server this is the channel it will join. 
-$server_chan = "#$short"; 
+$server_chan = "#$shortname"; 
 $meeting_log = "/var/www/html/TransparencyBot/".$shortname.'_'.date('Y-m-d').'.html';
 // log file to monitor bot performance
 global $debug_log;
