cdaec304d11e5301fa5a4ec4301cb6815fa4a7f0
Patrick McGuire <insidenothing@gmail.com>
2017-08-16 13:52:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
+<title>#mdpp Weekly Meetings</title>
+<div>Weekly Meeting Archive</div>
+<div>Click to open any meeting in a new tab</div>
 <?PHP
 $all = scandir('.');
 foreach ($all as $file) {
  $pos = strpos($file,'html');
  if ($pos !== false){
-  echo "<li><a href='$file'>$file</a></li>";
+  echo "<li><a target='_Blank' href='$file'>$file</a></li>";
  }
 }
 ?>
