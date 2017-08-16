8df7eaad0ce223df0b7deba37f205c9ec79b1cdd
Patrick McGuire <insidenothing@gmail.com>
2017-08-16 14:45:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?PHP include("config.php"); ?>
 <title><?PHP echo $server_chan;?> Weekly Meeting Vault</title>
+<h1><?PHP echo $server_chan;?> Weekly Meeting Vault</h1>
 <img src="<?PHP echo $logo;?>">
-<div><?PHP echo $server_chan;?> Weekly Meeting Vault</div>
 <div>Click to open any meeting in a new tab</div>
 <?PHP
 $all = scandir('.');
