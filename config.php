63516271eb57ab004da9c7306577836510153e0b
Patrick McGuire <insidenothing@gmail.com>
2017-08-16 09:52:23 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+// Shortname
+$shortname = "mdpp";
+//The server host is the IP or DNS of the IRC server. 
+$server_host = "irc.pirateirc.net"; 
+//Server Port, this is the port that the irc server is running on. Deafult: 6667 
+$server_port = 6667; 
+//Server Chanel, After connecting to the IRC server this is the channel it will join. 
+$server_chan = "#$short"; 
+?>
