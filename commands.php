c43809940a7adbb06e9e438a944cb9a540f98219
Patrick McGuire <insidenothing@gmail.com>
2017-08-17 12:49:24 -0400

    Update commands.php

--- commands.php
+++ commands.php
@@ -10,6 +10,9 @@ if ($pos !== false){
   SendCommand("PRIVMSG #mdpp :".$command_prefix."end meeting - Turn Off Meeting System\n\r");
   SendCommand("PRIVMSG #mdpp :".$command_prefix."start agenda - Turn On Agenda System\n\r");
   SendCommand("PRIVMSG #mdpp :".$command_prefix."end agenda - Turn Off Agenda System\n\r");
+  SendCommand("PRIVMSG #mdpp :".$command_prefix."history - Work in Progress\n\r");
+  SendCommand("PRIVMSG #mdpp :".$command_prefix."post - Work in Progress\n\r");
+  SendCommand("PRIVMSG #mdpp :".$command_prefix."email - work in Progress\n\r");
 }
 
 
@@ -49,7 +52,7 @@ $pos = strpos($server['READ_BUFFER'], $command_prefix.'start agenda');
 if ($pos !== false){  
   SendCommand("PRIVMSG #mdpp :Agenda log is on \n\r");  
   SendCommand("NAMES #mdpp\n\r");
-  touch($lockfile);
+  touch($lockfile_agenda);
   error_log("<div style='border:1px solid black; padding:5px; background-color:pink;'>".date('r')." Agenda Has Started</div>", 3, $meeting_log);
   $refresh = '<meta http-equiv="refresh" content="5">';
   error_log($refresh, 3, $meeting_log);
@@ -62,12 +65,41 @@ if ($pos !== false){
 $pos = strpos($server['READ_BUFFER'], $command_prefix.'end agenda');
 if ($pos !== false){ 
   SendCommand("PRIVMSG #mdpp :Agenda log is off \n\r");  
-  unlink($lockfile);
+  unlink($lockfile_agenda);
   error_log("<div style='border:1px solid black; padding:5px; background-color:pink;'>".date('r')." Agenda Complete </div>", 3, $meeting_log);
   foreach($speakers as $name => $count ){
     SendCommand("PRIVMSG #mdpp :Thank you $name for contributing $count messages to the meeting. \n\r");  
     error_log("<div style='border:1px solid black; padding:5px; background-color:pink;'>Thank you $name for contributing $count messages to the meeting.</div>", 3, $meeting_log);          
   }
+}
+
+
+
+//
+//// today's history
+//
+$pos = strpos($server['READ_BUFFER'], $command_prefix.'history');
+if ($pos !== false){ 
+  // we need to know who to send the history to...
+  SendCommand("PRIVMSG BaltimoreHacker :History Requested \n\r");  
+}
+
+//
+//// post the meeting to wordpress
+//
+$pos = strpos($server['READ_BUFFER'], $command_prefix.'post');
+if ($pos !== false){ 
+  // we need to know who to send the history to...
+  SendCommand("PRIVMSG BaltimoreHacker :History Requested \n\r");  
+}
+
+//
+//// send meeting via email
+//
+$pos = strpos($server['READ_BUFFER'], $command_prefix.'email');
+if ($pos !== false){ 
+  // we need to know who to send the history to...
+  SendCommand("PRIVMSG BaltimoreHacker :History Requested \n\r");  
 }
 
 
