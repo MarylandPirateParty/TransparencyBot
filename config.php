fea6db9b559ce4ef8b698f3970eab69b0d7eb511
Patrick McGuire <insidenothing@gmail.com>
2017-08-17 12:42:58 -0400

    Update config.php

--- config.php
+++ config.php
@@ -17,6 +17,7 @@ $agenda_log = "/var/www/html/TransparencyBot/".$shortname.'_agenda_'.date('Y-m-d
 global $debug_log;
 $debug_log = "/var/www/html/TransparencyBot/debug.log"; 
 $lockfile = "/var/www/html/TransparencyBot/meeting_active.info";
+$lockfile_agenda = "/var/www/html/TransparencyBot/agenda_active.info";
 $logo = "/wp-content/uploads/2017/07/MarylandPiratePartySmall.png";
 // who talks
 global $speakers;
