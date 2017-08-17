33a3f53b555c8a61e1910c00f73084bdb81fe149
Patrick McGuire <insidenothing@gmail.com>
2017-08-17 09:55:28 -0400

    Update commands.php

--- commands.php
+++ commands.php
@@ -1,9 +1,23 @@
 <?PHP
+//
+//// start command
+//
+$pos = strpos($server['READ_BUFFER'], '+help');
+if ($pos !== false){  
+  SendCommand("PRIVMSG #mdpp :I have made the following public commands available \n\r");
+  SendCommand("PRIVMSG #mdpp :+help - You just ran that command \n\r");
+  SendCommand("PRIVMSG #mdpp :+start meeting - Turn On Meeting System\n\r");
+  SendCommand("PRIVMSG #mdpp :+end meeting - Turn Off Meeting System\n\r");
+  SendCommand("PRIVMSG #mdpp :+start agenda - Turn On Agenda System\n\r");
+  SendCommand("PRIVMSG #mdpp :+end agenda - Turn Off Agenda System\n\r");
+}
+
+
 
 //
 //// start command
 //
-$pos = strpos($server['READ_BUFFER'], 'start meeting');
+$pos = strpos($server['READ_BUFFER'], '+start meeting');
 if ($pos !== false){  
   SendCommand("PRIVMSG #mdpp :Meeting log has started \n\r");  
   SendCommand("NAMES #mdpp\n\r");
@@ -15,9 +29,9 @@ if ($pos !== false){
 
 
 //
-//// end command
+//// end meeting
 //
-$pos = strpos($server['READ_BUFFER'], 'end meeting');
+$pos = strpos($server['READ_BUFFER'], '+end meeting');
 if ($pos !== false){ 
   SendCommand("PRIVMSG #mdpp :Meeting log has ended \n\r");  
   unlink($lockfile);
@@ -27,5 +41,34 @@ if ($pos !== false){
     error_log("<div style='border:1px solid black; padding:5px; background-color:lightblue;'>Thank you $name for contributing $count messages to the meeting.</div>", 3, $meeting_log);          
   }
 }
+
+//
+//// start agenda
+//
+$pos = strpos($server['READ_BUFFER'], '+start agenda');
+if ($pos !== false){  
+  SendCommand("PRIVMSG #mdpp :Agenda log is on \n\r");  
+  SendCommand("NAMES #mdpp\n\r");
+  touch($lockfile);
+  error_log("<div style='border:1px solid black; padding:5px; background-color:pink;'>".date('r')." Agenda Has Started</div>", 3, $meeting_log);
+  $refresh = '<meta http-equiv="refresh" content="5">';
+  error_log($refresh, 3, $meeting_log);
+}
+
+
+//
+//// end agenda
+//
+$pos = strpos($server['READ_BUFFER'], '+end agenda');
+if ($pos !== false){ 
+  SendCommand("PRIVMSG #mdpp :Agenda log is off \n\r");  
+  unlink($lockfile);
+  error_log("<div style='border:1px solid black; padding:5px; background-color:pink;'>".date('r')." Agenda Complete </div>", 3, $meeting_log);
+  foreach($speakers as $name => $count ){
+    SendCommand("PRIVMSG #mdpp :Thank you $name for contributing $count messages to the meeting. \n\r");  
+    error_log("<div style='border:1px solid black; padding:5px; background-color:pink;'>Thank you $name for contributing $count messages to the meeting.</div>", 3, $meeting_log);          
+  }
+}
+
 
 ?>
