c65ecefaba6508a8f1460226f3582dfffcf5eb0d
Patrick McGuire <insidenothing@gmail.com>
2017-08-17 12:51:05 -0400

    Update log.php

--- log.php
+++ log.php
@@ -28,4 +28,37 @@ if(file_exists($lockfile) && $i > 100){
 }else{
   error_log(date('r')." [receive] [$i] ".$server['READ_BUFFER'], 3, $debug_log);
 }
+
+
+if(file_exists($lockfile_agenda) && $i > 100){
+  $parts = explode("!",$server['READ_BUFFER']);
+  $name = $parts[0];
+  $parts = explode(":",$server['READ_BUFFER']); 
+  $message = $parts[2];
+  if (trim($message) != '') {  
+      // ignore system messages in meeting log
+      $pos_t1 = strpos($server['READ_BUFFER'], 'MDPPbot');
+      $pos_t2 = strpos($server['READ_BUFFER'], 'PING');
+      $pos_t3 = strpos($server['READ_BUFFER'], 'ChanServ');
+      if ($pos_t1 === false && $pos_t2 === false){
+        if(isset($speakers[$name])){
+            $speakers[$name] = $speakers[$name] + 1;
+        }else{
+            $speakers[$name] = 0;
+            SendCommand("PRIVMSG #mdpp :Welcome $name, could you tell your name and county to the group for the current meeting that is in progress.\n\r");  
+            sleep(1);
+            error_log("<div style='border:1px solid black; padding:5px;'>Maryland Pirate Party Welcomes You $name, could you tell your name and county to the group for the current meeting that is in progress.</div>", 3, $agenda_log); 
+        }
+        error_log("<div style='border:1px solid black; padding:5px;'>".date('r')." <b>$name:</b> $message </div>", 3, $agenda_log); 
+      }
+      if ($pos_t3 !== false){
+        error_log("<div style='border:1px solid black; padding:5px; background-color:orange;'>Pirates in Attendance ".$server['READ_BUFFER']." </div>", 3, $agenda_log); 
+      }
+  }
+  error_log(date('r')." [meeting active] [$i] ".$server['READ_BUFFER'], 3, $debug_log);
+}
+
+
+
+
 ?>
