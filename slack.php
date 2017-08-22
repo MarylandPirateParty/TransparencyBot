8d7a0c38fc63355dabd4984ad1ece0aa77cc726b
Patrick McGuire <insidenothing@gmail.com>
2017-08-22 11:21:15 -0400

    Update slack.php

--- slack.php
+++ slack.php
@@ -5,5 +5,6 @@ $response=array('body');
 $type = $_POST['body']['type'];
 $token = $_POST['body']['token'];
 $challenge = $_POST['body']['challenge'];
-echo $challenge;
+//echo $challenge;
+error_log(date('r')." [slace receive] [$type] [$token] [$challenge] \n", 3, $debug_log);
 ?>
