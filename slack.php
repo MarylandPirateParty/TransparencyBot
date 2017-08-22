5f390c0576011d14e30b2ba154ef9efa7b82789b
Patrick McGuire <insidenothing@gmail.com>
2017-08-22 11:51:14 -0400

    Update slack.php

--- slack.php
+++ slack.php
@@ -2,8 +2,8 @@
 include("config.php");
 error_log(date('r')." [slack receive] [begin] [".$_SERVER['REQUEST_METHOD']."] \n", 3, $slack_log); 
 // use a buffer file fo imcomming post messages from slack
-$buffer = stream_get_contents(STDIN);
-error_log(date('r')." [slack receive] [".$buffer."] \n", 3, $slack_log);
+print_r($_POST);
+//error_log(date('r')." [slack receive] [".$buffer."] \n", 3, $slack_log);
 //foreach ($_POST as $key) {
     //echo "Key: $key<br />\n";
 //    error_log(date('r')." [slack receive] [$key] \n", 3, $slack_log); 
