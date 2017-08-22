9211b769d5f2d28d82c95d825b9134de819b64a7
Patrick McGuire <insidenothing@gmail.com>
2017-08-22 11:22:16 -0400

    Update slack.php

--- slack.php
+++ slack.php
@@ -1,11 +1,10 @@
 <?PHP
 include("config.php");
-$response=array('body');
 // use a buffer file fo imcomming post messages from slack
 $type = $_POST['body']['type'];
 $token = $_POST['body']['token'];
 $challenge = $_POST['body']['challenge'];
 //echo $challenge;
-error_log(date('r')." [slace receive] [$type] [$token] [$challenge] \n", 3, $debug_log);
+error_log(date('r')." [slack receive] [$type] [$token] [$challenge] \n", 3, $debug_log);
 echo "test";
 ?>
