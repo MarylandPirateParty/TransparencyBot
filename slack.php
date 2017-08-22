949f462ebde958d39c7910184252fa61389f4d17
Patrick McGuire <insidenothing@gmail.com>
2017-08-22 12:05:50 -0400

    Update slack.php

--- slack.php
+++ slack.php
@@ -4,10 +4,8 @@ $response=array();
 $response['challenge']='test';
 error_log(date('r')." [slack receive] [begin] [".$_SERVER['REQUEST_METHOD']."] \n", 3, $slack_log); 
 // use a buffer file fo imcomming post messages from slack
-ob_start();
-file_get_contents('php://input') or error_log(date('r')." [slack receive] [failed to open buffer] \n", 3, $slack_log);;
-$buffer = ob_get_clean();
-error_log(date('r')." [slack receive] [".$buffer."] \n", 3, $slack_log);
+$data = json_decode(file_get_contents('php://input'), true);
+error_log(date('r')." [slack receive] [".$data."] \n", 3, $slack_log);
 //foreach ($_POST as $key) {
     //echo "Key: $key<br />\n";
 //    error_log(date('r')." [slack receive] [$key] \n", 3, $slack_log); 
